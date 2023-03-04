de6d8ac7123e915a988aeed68a436e9fe339e165
janis147 <30315585+janis147@users.noreply.github.com>
2023-03-04 13:18:26 +0000

    wefwef

--- tree_height.py
+++ tree_height.py
@@ -33,10 +33,11 @@ def input_from_keyboard():
     try:
         n = input()
         parents = input()
+        print(str(n)+" "+str(parents))
         parents = parents.split(" ")
         return n, parents
     except:
-        print("ERROR1")
+        print(str(n)+" "+str(parents))
         n = 0
         parents = 0
         return n, parents
