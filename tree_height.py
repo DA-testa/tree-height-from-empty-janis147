a8342cdb8423c167902edd8bb952414f7b75b5ab
janis147 <30315585+janis147@users.noreply.github.com>
2023-03-04 16:35:40 +0000

    awdawd

--- tree_height.py
+++ tree_height.py
@@ -1,3 +1,5 @@
+# python3
+
 import sys
 import threading
 import numpy as np
@@ -5,6 +7,7 @@ import numpy as np
 def compute_height(n, parents):
     heights = np.zeros(int(n))
     max_height = 0
+
     for i in range(int(n)):
         if heights[i] > 0:
             continue
@@ -17,43 +20,45 @@ def compute_height(n, parents):
             else:
                 height += 1
                 j = int(parents[j])
+
         heights[i] = height
         if height > max_height:
             max_height = height
     return max_height
 
-def input_from_keyboard(inputs):
-    n = inputs[1]
-    parents = inputs[2].split()
+def input_from_keyboard():
+    n = input()
+    print(n+"\n")
+    parents = input()
+    print(parents+"\n")
+    parents = parents.split(" ")
     return n, parents
 
 def input_from_file(file_dir):
     try:
         with open(f"./test/{file_dir}") as f:
-            contents = f.read()
+            contents = f.readlines()
     except:
         print("ERROR")
         return
 
-    inputs = contents.split("\\r\\n")
-    n = inputs[1]
-    parents = inputs[2].split()
+    n = contents[0].strip("\n")
+    parents = contents[1].strip("\n").split(" ")
     f.close()
     return n, parents
 
-
 def main():
-    inputs = input().strip().split('\\r\\n')
-    if inputs[0] == "F":
-        file_dir = inputs[1]
+    input_method = input()
+    if input_method.strip(" ") == "F":
+        file_dir = input()
         if str(file_dir[-1]) != "a":
             n, parents = input_from_file(file_dir)
             if n and parents:
                 height = compute_height(n, parents)
                 print(int(height))
     else:
-        if inputs[0] == "I":
-            n, parents = input_from_keyboard(inputs)
+        if input_method.strip(" ") == "I":
+            n, parents = input_from_keyboard()
             if n and parents:
                 height = compute_height(n, parents)
                 print(int(height))
