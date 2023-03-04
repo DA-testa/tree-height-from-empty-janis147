8a27fc0487150a581d0431997d766d2b94b01a92
janis147 <30315585+janis147@users.noreply.github.com>
2023-03-04 16:17:23 +0000

    mby works

--- tree_height.py
+++ tree_height.py
@@ -1,18 +1,13 @@
-# python3
-
 import sys
 import threading
 import numpy as np
 
-
 def compute_height(n, parents):
     heights = np.zeros(int(n))
     max_height = 0
-
     for i in range(int(n)):
         if heights[i] > 0:
             continue
-
         height = 0
         j = i
         while j != -1:
@@ -22,17 +17,14 @@ def compute_height(n, parents):
             else:
                 height += 1
                 j = int(parents[j])
-
         heights[i] = height
         if height > max_height:
             max_height = height
-
     return max_height
 
-def input_from_keyboard():
-    n = input()
-    parents = input()
-    parents = parents.split(" ")
+def input_from_keyboard(inputs):
+    n = inputs[1]
+    parents = inputs[2].split()
     return n, parents
 
 def input_from_file(file_dir):
@@ -40,6 +32,7 @@ def input_from_file(file_dir):
         with open(f"./test/{file_dir}") as f:
             contents = f.readlines()
     except:
+        print("ERROR")
         return
 
     n = contents[0].strip("\n")
@@ -48,17 +41,17 @@ def input_from_file(file_dir):
     return n, parents
 
 def main():
-    input_method = input()
-    if input_method.strip(" ") == "F":
-        file_dir = input()
+    inputs = input().strip().split('\\r\\n')
+    if inputs[0] == "F":
+        file_dir = inputs[1]
         if str(file_dir[-1]) != "a":
             n, parents = input_from_file(file_dir)
             if n and parents:
                 height = compute_height(n, parents)
                 print(int(height))
     else:
-        if input_method.strip(" ") == "I":
-            n, parents = input_from_keyboard()
+        if inputs[0] == "I":
+            n, parents = input_from_keyboard(inputs)
             if n and parents:
                 height = compute_height(n, parents)
                 print(int(height))
@@ -66,4 +59,3 @@ def main():
 sys.setrecursionlimit(10 ** 7)  # max depth of recursion
 threading.stack_size(2 ** 27)  # new thread will get stack of such size
 threading.Thread(target=main).start()
-
