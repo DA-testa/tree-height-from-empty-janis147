a5ee9841ca540a6deeb9cb37670fb846cd3a738a
janis147 <30315585+janis147@users.noreply.github.com>
2023-03-04 16:58:54 +0000

    FINAL

--- tree_height.py
+++ tree_height.py
@@ -7,7 +7,6 @@ import numpy as np
 def compute_height(n, parents):
     heights = np.zeros(int(n))
     max_height = 0
-
     for i in range(int(n)):
         if heights[i] > 0:
             continue
@@ -20,17 +19,18 @@ def compute_height(n, parents):
             else:
                 height += 1
                 j = int(parents[j])
-
         heights[i] = height
         if height > max_height:
             max_height = height
     return max_height
 
 def input_from_keyboard():
-    n = input()
-    parents = input()
-    parents = parents.split(" ")
-    return n, parents
+    n = input().strip()
+    if n:
+        parents = input().strip().split(" ")
+        if parents:
+            return n, parents
+    return None, None
 
 def input_from_file(file_dir):
     try:
@@ -38,28 +38,30 @@ def input_from_file(file_dir):
             contents = f.readlines()
     except:
         print("ERROR")
-        return
+        return None, None
 
-    n = contents[0].strip("\n")
-    parents = contents[1].strip("\n").split(" ")
-    f.close()
-    return n, parents
+    n = contents[0].strip()
+    if n:
+        parents = contents[1].strip().split(" ")
+        if parents:
+            f.close()
+            return n, parents
+    return None, None
 
 def main():
-    input_method = input()
-    if input_method.strip(" ") == "F":
-        file_dir = input()
+    input_method = input().strip()
+    if input_method == "F":
+        file_dir = input().strip()
         if str(file_dir[-1]) != "a":
             n, parents = input_from_file(file_dir)
             if n and parents:
                 height = compute_height(n, parents)
                 print(int(height))
-    else:
-        if input_method.strip(" ") == "I":
-            n, parents = input_from_keyboard()
-            if n and parents:
-                height = compute_height(n, parents)
-                print(int(height))
+    elif input_method == "I":
+        n, parents = input_from_keyboard()
+        if n and parents:
+            height = compute_height(n, parents)
+            print(int(height))
 
 sys.setrecursionlimit(10 ** 7)  # max depth of recursion
 threading.stack_size(2 ** 27)  # new thread will get stack of such size
